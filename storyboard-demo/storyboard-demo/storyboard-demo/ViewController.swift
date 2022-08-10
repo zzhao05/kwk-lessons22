8641a1e85869ef31f1a2231328fe3ae4f71a01c3
zzhao05 <110996555+zzhao05@users.noreply.github.com>
2022-08-10 11:34:00 -0400

    storyboard-demo

---
+++ storyboard-demo/storyboard-demo/storyboard-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyboard-demo
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
